01ef60131bf501374409bcadc555c8513ec057fa
El Hedri <elhed001@login02.mogon>
2016-06-21 14:44:42 +0200

    Adding a list of env variables. Finished writing the run_madgraph.sh but no debugging so far.

---
+++ scripts/env_file.sh
@@ -0,0 +1,5 @@
+# Environment variables 
+
+SCRATCH=/jobdir/$LSB_JOBID/
+TAR_DIR=/home/elhed001/
+$MADGRAPH=MG5_aMC_V2_3_2_2
--- scripts/run_madgraph.sh
+++ scripts/run_madgraph.sh
@@ -1,5 +1,18 @@
 !/bin/bash
 
-source env_files.sh
+source env_file.sh
 
-cp $MADGRAPH.tar.gz 
+local=$PWD
+cp $TAR_DIR/$MADGRAPH.tar.gz $SCRATCH/
+tar -xzf $SCRATCH/$MADGRAPH.tar.gz
+rm $SCRATCH/$MADGRAPH.tar.gz
+cp $1/*card*.dat $SCRATCH/$MADGRAPH/
+cd $SCRATCH/$MADGRAPH
+./bin/mg5_aMC proc_card_mg5.dat
+model=`grep ^output proc_card_mg5.dat | awk '{print $2}'`
+cp run_card.dat $model
+cp param_card_$2.dat $model/param_card.dat
+cd $model/Events/
+../bin/generate_events 0 run_01
+cd $local
+rm -r $SCRATCH
